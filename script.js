133bebcb1675e0f250ecf0ca477c814a072a6f15
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-05 15:26:25 +0000

    js edited

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.getElementById("determine").addEventListener("click", myfunction);
-function myfunction() {
-  let number;
+document.getElementById('checknumber').addEventListener('click', myfunction)
+function myfunction () {
+  const number
   number = document.getElementById('usernumber').value
 
   if (number > 0) {
-    alert('Your number is positive');
-  }
+    alert('Your number is positive')
+    }
   else if (number < 0) {
-    alert('Your number is negative');
+    alert('Your number is negative')
+    }
   }
-}
