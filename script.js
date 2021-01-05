f6bc9c6ee01eeae053f810cb62cd6cfd93971561
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-05 15:54:42 +0000

    curly brace

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function myfunction () {
   const number = document.getElementById('usernumber').value
 
   if (number > 0) {
-    alert('Your number is positive')}
+    alert('Your number is positive') }
   else if (number < 0) {
     alert('Your number is negative')
   }
