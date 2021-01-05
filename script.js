7b665c92d1468ceafd861109e3258ff59d00da27
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-05 15:08:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("determine").addEventListener("click", myfunction);
+function myfunction() {
+  let number;
+  number = document.getElementById('usernumber').value
+
+  if (number > 0) {
+    alert('Your number is positive');
+  }
+  else if (number < 0) {
+    alert('Your number is negative');
+  }
+}
